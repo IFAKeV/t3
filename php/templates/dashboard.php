afb4c2ca896ac316fc064bd3c396c1b71b2bb97f
IFAKeV <edv@ifak-bochum.de>
2025-07-12 18:24:18 +0200

    Update dashboard.php

--- php/templates/dashboard.php
+++ php/templates/dashboard.php
@@ -23,7 +23,7 @@
             <tbody>
                 <?php foreach ($tickets as $ticket): ?>
                 <tr>
-                    <td><a href="index.php?action=view_ticket&id=<?php echo $ticket['TicketID']; ?>">#<?php echo $ticket['TicketID']; ?></a></td>
+                    <td><a href="index.php?action=view_ticket&id=<?php echo $ticket['TicketID']; ?>"><?php echo $ticket['TicketID']; ?></a></td>
                     <td><span class="team-badge" style="background-color: <?php echo htmlspecialchars($ticket['TeamColor']); ?>;"><?php echo htmlspecialchars($ticket['TeamName']); ?></span></td>
                     <td><span class="status-badge" style="background-color: <?php echo htmlspecialchars($ticket['StatusColor']); ?>;"><?php echo htmlspecialchars($ticket['StatusName']); ?></span></td>
                     <td><span class="priority-badge" style="background-color: <?php echo htmlspecialchars($ticket['PriorityColor']); ?>;"><?php echo htmlspecialchars($ticket['PriorityName']); ?></span></td>
